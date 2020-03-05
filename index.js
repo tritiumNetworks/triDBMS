277ff71c4bda0e35f85e5c15c8cd81d0514ba3a2
PMHStudio <pmhstudio.pmh@gmail.com>
2020-03-05 13:37:08 +0900

    SSL Added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const PORT = process.env.triDBMSPort || 8080
+const PORT = process.env.triDBMSPort || 8443
 
 const cors = require('cors')
 const path = require('path').resolve()
+const https = require('https')
 const express = require('express')
 const apiRouter = require('./router/apiRouter')
+const { readFileSync } = require('fs')
 
 const app = express()
+const ssl = { cert: readFileSync(path + '/cert/trinets-cert.pem'), key: readFileSync(path + '/cert/trinets-key.pem') }
+
 app.use(cors())
 app.use('/static', express.static(path + '/datas'))
 
@@ -13,4 +17,4 @@ app.get('/', (_req, res) => res.redirect('/api'))
 app.get('/api', apiRouter)
 app.get('/api/:apiv', apiRouter)
 
-app.listen(PORT, () => console.log('Server is now on http://localhost:' + PORT))
+https.createServer(ssl, app).listen(PORT, () => { console.log('SSL Server is now on https://localhost:433') })
