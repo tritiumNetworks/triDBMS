41afed8b4fb6145e9a57e13fabbc8dc9119b3bc2
PMHStudio <pmhstudio.pmh@gmail.com>
2020-03-11 15:07:52 +0900

    Document Added

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ const ssl = { cert: readFileSync(path + '/cert/trinets-cert.pem'), key: readFile
 
 app.use(cors())
 app.use('/static', express.static(path + '/datas'))
+app.use('/docs', express.static(path + '/docs'))
 
-app.get('/', (_req, res) => res.redirect('/api'))
+app.get('/', (_req, res) => res.redirect('/docs'))
 app.get('/api', apiRouter)
 app.get('/api/:apiv', apiRouter)
 
-https.createServer(ssl, app).listen(PORT, () => { console.log('SSL Server is now on https://localhost:433') })
+https.createServer(ssl, app).listen(PORT, () => { console.log('SSL Server is now on https://localhost:' + PORT) })
