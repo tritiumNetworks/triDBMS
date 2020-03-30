7623fe57c5cf386be33efec7a5763052a63fe5a8
pmh-only <pmhstudio.pmh@gmail.com>
2020-03-30 19:26:46 +0900

    GoLang Version

--- router/apiRouter.js
+++
@@ -1,9 +0,0 @@
-const apiHandler = require('../handler/apiHandler')
-
-function fnc (req, res) {
-  const apiv = req.params.apiv || 'v1'
-  if (Object.keys(apiHandler.apis).includes(apiv)) apiHandler.apis[apiv](req, res)
-  else res.sendStatus(404)
-}
-
-module.exports = fnc
